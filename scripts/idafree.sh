9b4b65ec0fd3800c4a171380fe933f6616b1dd66
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2026-07-21 12:36:58 +0530

    auto-sync: 2026-07-21 12:36

---
+++ scripts/idafree.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nohup ~/ida-free-9.4/ida > /dev/null 2>&1 &
+echo "ida free is now runing..."
---
+++ scripts/liberpods.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nohup ./librepods-x86_64.AppImage > /dev/null 2>&1 &
+echo "LiberPods Running in Backgroung"
